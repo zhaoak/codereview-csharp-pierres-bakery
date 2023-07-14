9eac270fc5a98744a243f32e4515b14021db17af
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-14 10:34:01 -0700

    all bread purchase tests passing

--- Bakery.Tests/ModelTests/BakedGoodTests.cs
+++ Bakery.Tests/ModelTests/BakedGoodTests.cs
@@ -28,5 +28,27 @@ namespace Bakery.Tests
 
             Assert.AreEqual(expectedCost, testBread.Purchase(quantityToPurchase));
         }
+
+        [TestMethod]
+        public void Bread_PurchaseEight_Int()
+        {
+            int quantityToPurchase = 8;
+            Bread testBread = new Bread();
+            
+            int expectedCost = 30;
+
+            Assert.AreEqual(expectedCost, testBread.Purchase(quantityToPurchase));
+        }
+
+        [TestMethod]
+        public void Bread_PurchaseFive_Int()
+        {
+            int quantityToPurchase = 5;
+            Bread testBread = new Bread();
+            
+            int expectedCost = 20;
+
+            Assert.AreEqual(expectedCost, testBread.Purchase(quantityToPurchase));
+        }
     }
 }
