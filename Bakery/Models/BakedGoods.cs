ee2a718c4618553649daf7300ab66ed6c73f17bd
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-14 11:31:30 -0700

    added PriceCheck function to all classes

--- Bakery/Models/BakedGoods.cs
+++ Bakery/Models/BakedGoods.cs
@@ -2,12 +2,12 @@
 namespace Bakery.Models
 {
     public abstract class BakedGood {
-        // Price and Bogo are set to default values in inherited class 
-        // constructors
+        // Price and Bogo are set in inherited class constructors
         public int Price { get; set; }
         public int Bogo { get; set; } // how many units purchased it takes to get one free
 
         // returns cost of purchase
-        public abstract int Purchase(int quantity);
+        public abstract int Purchase(int quantity); // returns cost of purchasing `quantity` items, adds to static list of orders
+        public abstract int PriceCheck(int quantity); // like Purchase(), but does not add to list of orders
     }
 }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -9,7 +9,12 @@ namespace Bakery.Models
 
         public override int Purchase(int quantity) {
             int cost = (Price * quantity) - Price * (quantity / (Bogo + 1));
+            // also add order to static class list of orders
             return cost;
+        }
+
+        public override int PriceCheck(int quantity) {
+            return (Price * quantity) - Price * (quantity / (Bogo + 1));
         }
     }
 }
