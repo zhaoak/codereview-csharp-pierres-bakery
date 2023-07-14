8b44570af0bdd913c99cfcb76882fa2e2ba741c3
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-14 14:15:57 -0700

    refactored to have Bread and Pastry use inheritance

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -7,12 +7,6 @@ namespace Bakery.Models
             Bogo = 2;
         }
 
-        public override int Purchase(int quantity) {
-            int cost = (Price * quantity) - Price * (quantity / (Bogo + 1));
-            // also add order to static class list of orders
-            return cost;
-        }
-
         public override int PriceCheck(int quantity) {
             return (Price * quantity) - Price * (quantity / (Bogo + 1));
         }
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -6,12 +6,6 @@ namespace Bakery.Models
             Bogo = 3;
         }
 
-    public override int Purchase(int quantity) {
-        int cost = (Price * quantity) - Price * (quantity / (Bogo + 1));
-        // also add order to static class list of orders
-        return cost;
-    }
-
     public override int PriceCheck(int quantity) {
         return (Price * quantity) - Price * (quantity / (Bogo + 1));
     }
